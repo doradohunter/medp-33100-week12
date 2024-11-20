ffcdde031cc8df1d8b321e951264a05fcb6ed1db
Rochelle Williams <rochelle.williams80@myhunter.cuny.edu>
2024-11-20 15:10:11 -0500

    added data from database

--- config/db.js
+++ config/db.js
@@ -11,12 +11,10 @@ const client = new MongoClient(uri, {
 });
 async function connectToDatabase() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
-        // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
+        return client.db('magic_association');
+    } catch {
         // Ensures that the client will close when you finish/error
         await client.close();
     }
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,37 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Mage Association Employee Database' });
+router.get('/', async function (req, res, next) {
+
+  try {
+    const db = req.app.locals.db;
+    const employees = await db.collection('employees')
+      .aggregate([
+        {
+          $lookup: {
+            from: 'departments',
+            localField: 'depID',
+            foreignField: '_id',
+            as: 'department'
+          }
+        },
+        {
+          $lookup: {
+            from: 'titles',
+            localField: 'titleID',
+            foreignField: '_id',
+            as: 'title'
+          }
+        }
+      ])
+      .toArray();
+    res.render('index', { title: 'Magic Association Employee Database', employees: employees });
+  } catch (error) {
+    console.log('error!');
+    res.status(500).send('An error occurred');
+  }
+
+
 });
 
 module.exports = router;
