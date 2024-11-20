3ef6c22269b11e308cfdc9ed697c21e195cf1aa6
ExtraPolite <imextrapolite2@gmail.com>
2024-11-20 16:21:32 -0500

    display information

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', async function (req, res, next) {
   try {
-    res.render('index', { title: 'Express' });
-
     const db = req.app.locals.db;
     const habits = await db.collection('habits').find().toArray();
-
-    console.log(habits);
+    res.render('index', { title: 'Express', habits: habits });
   } catch (error) {
     console.log(error);
   }
