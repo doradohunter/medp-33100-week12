7d5efa168c978a0744ef5d3e6ac491a3e5b48bd9
Arlenis Pena <101661696+isap630@users.noreply.github.com>
2024-11-21 12:06:37 -0500

    updated Library

---
+++ routes/index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', async function(req, res, next) {
+  if(req.app.locals.db){
+    try{
+      const db = req.app.locals.db;
+      const books = await db.collection('Books')
+        .aggregate([
+          {
+            $lookup: {
+              from: 'Members',
+              localField: '_id',
+              foreignField:'borrowedBooks',
+              as: 'borrowers'
+            }
+          }
+        ])
+        .toArray();
+        console.log(books[4])
+        console.log(books[4].borrowers)
+        res.render('index', { title: 'Library', books: books});
+    } catch(error){
+      console.log(error);
+    }
+  }
+});
+
+module.exports = router;
