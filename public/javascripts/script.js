1e73ed3776033e4587650803af1ec33945781b1e
Judy Zhang <101025113+JudyZhang05@users.noreply.github.com>
2024-11-22 14:13:24 -0500

    post req sucess and implement shopping cart

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,16 +1,54 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let clickedProduct = ''
+    //select product user clicked
     const allProducts = document.querySelectorAll('.col');
-    allProducts.forEach((product) => {
-        product.addEventListener('click', (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-            clickedProduct = e.target.parentElement.innerHTML.split('<p class="name">')[1].slice(0,-255);
+    allProducts.forEach(products => {
+        let itemName, itemPrice, itemRating, itemStock = ''
+        const items = products.querySelectorAll('.item');
+        items.forEach(itemDets => {
+            itemName = itemDets.querySelector('.name');
+            itemPrice = itemDets.querySelector('.price');
+        })
+        const dets = products.querySelectorAll('.pDets');
+        dets.forEach(pdets => {
+            itemStock = pdets.querySelector('.stock');
+            itemRating = pdets.querySelector('.rate');
+        })
+        products.addEventListener('click', () => {
+            // console.log(itemName.innerHTML,
+            //     itemPrice.innerHTML,
+            //     itemRating.innerHTML.slice(0, 3),
+            //     itemStock.innerHTML.slice(0, 2)
+            //     )
+            // console.log(products.id)
+
+            const addToCart = {
+                productID: products.id
+            }
+            console.log(addToCart)
+            fetch('/', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(addToCart)
+            });
+
         })
     })
 
-    // const name = document.querySelector('candleName');
-    // const rate = document.querySelector('candleRate');
-    // const price = document.querySelector('candlePrice');
 
+
+
+    // const candleName = document.querySelector('.candleName');
+    // const rate = document.querySelector('.candleRate');
+    // const price = document.querySelector('.candlePrice');
+
+
+    
 })
+
+// async function updateStock(productID){
+//         fetch('/posts', {
+//             method: 'PUT'
+//         })
+//     }
--- routes/index.js
+++ routes/index.js
@@ -8,13 +8,44 @@ router.get('/', async function(req, res, next) {
   try{
     const db = req.app.locals.db;
     const products = await db.collection('products')
-      .find()
-      .toArray();
+      .aggregate([
+        {
+          $lookup: {
+            from: 'categories'
+            ,foreignField: '_id'
+            ,localField: 'category'
+            ,as: 'label'
+          }
+        }
+      ])
+      .toArray()
     res.render('index', { products: products });
 
   }catch (error) {
     console.log(error)
   }
+});
+
+router.post('/', async function (req, res, next) {
+  console.log('MAKING POST REQUEST')
+  console.log(req.body)
+  // try {
+  //     const db = req.app.locals.db;  // Access the shared database instance
+  //     const post = req.body;
+  //     console.log(post);
+
+  //     const newPost = {
+  //         title: post.title,
+  //         content: post.content,
+  //         authorID: new ObjectId(post.authorID),
+  //         createdAt: new Timestamp({ t: Math.floor(Date.now() / 1000), i: 0 }),
+  //     };
+
+  //     await db.collection('posts').insertOne(newPost);
+  //     res.send('Created new post');
+  // } catch (error) {
+  //     next(error);
+  // }
 });
 
 module.exports = router;
